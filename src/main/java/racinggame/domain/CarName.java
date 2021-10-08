84739e888799f2e532affb6e9d959b52013e9d96
Wangki Lee <lwk3007@gmail.com>
2021-10-09 00:25:09 +0900

    feat: 자동차 이름 생성 예외 케이스 추가
    - 생성자에서 이름 검증
    - 검증 메서드 private 으로 전환

--- src/main/java/racinggame/domain/CarName.java
+++ src/main/java/racinggame/domain/CarName.java
@@ -3,8 +3,18 @@ package racinggame.domain;
 public class CarName {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 5;
+    public static final String VALIDATE_CAR_NAME_MESSAGE = "[ERROR] 자동차 이름은 1자리 이상 5자리 이하로 만들 수 있습니다.";
 
-    public static boolean validateName(String name) {
-        return name.length() >= MIN_NUMBER && name.length() <= MAX_NUMBER;
+    private final String name;
+
+    public CarName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() < MIN_NUMBER || name.length() > MAX_NUMBER) {
+            throw new IllegalArgumentException(VALIDATE_CAR_NAME_MESSAGE);
+        }
     }
 }
--- src/test/java/racinggame/domain/CarNameTest.java
+++ src/test/java/racinggame/domain/CarNameTest.java
@@ -1,17 +1,29 @@
 package racinggame.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarNameTest {
+    public static final String VALIDATE_CAR_NAME_MESSAGE = "[ERROR] 자동차 이름은 1자리 이상 5자리 이하로 만들 수 있습니다.";
+
     @Test
     @DisplayName("자동차_이름_1_5_검증")
     void validate_car_name_1_5() {
-        assertTrue(CarName.validateName("t"));
-        assertTrue(CarName.validateName("test1"));
-        assertFalse(CarName.validateName("test12"));
-        assertFalse(CarName.validateName(""));
+        new CarName("t");
+        new CarName("test1");
+    }
+
+    @Test
+    @DisplayName("자동차_이름_예외")
+    void validate_car_name_exception() {
+        assertThatThrownBy(() -> new CarName(""))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(VALIDATE_CAR_NAME_MESSAGE);
+
+        assertThatThrownBy(() -> new CarName("test12"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(VALIDATE_CAR_NAME_MESSAGE);
     }
 }
