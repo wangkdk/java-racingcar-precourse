bc4a5319777d50e808a5f97ec02b9c2b8616392c
Wangki Lee <lwk3007@gmail.com>
2021-10-11 22:20:14 +0900

    feat: 경기횟수 입력 최소값 추가
    최소 1회 이상이여야 함

--- src/main/java/racinggame/view/RacingInputView.java
+++ src/main/java/racinggame/view/RacingInputView.java
@@ -7,9 +7,11 @@ import java.util.List;
 import nextstep.utils.Console;
 
 public class RacingInputView {
+    private static final int PLAY_COUNT_MIN_NUMBER = 1;
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_PLAY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String VALID_PLAY_COUNT_MESSAGE = "[ERROR] 경주 횟수는 숫자로만 입력 가능합니다.";
+    private static final String VALID_PLAY_COUNT_MIN_MESSAGE = "[ERROR] 경주 횟수는 1회 이상이여야 합니다.";
     private static final String COMMA = ",";
 
     private RacingInputView() {
@@ -24,13 +26,24 @@ public class RacingInputView {
 
     public static int inputPlayCount() {
         System.out.println(INPUT_PLAY_COUNT_MESSAGE);
-        String playCount = Console.readLine();
+        String inputPlayCount = Console.readLine();
         System.out.println();
         try {
-            return Integer.parseInt(playCount);
+            int playCount = Integer.parseInt(inputPlayCount);
+            validatePlayCount(playCount);
+            return playCount;
         } catch (NumberFormatException exception) {
             System.out.println(VALID_PLAY_COUNT_MESSAGE);
             return inputPlayCount();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputPlayCount();
+        }
+    }
+
+    private static void validatePlayCount(int playCount) {
+        if (playCount < PLAY_COUNT_MIN_NUMBER) {
+            throw new IllegalArgumentException(VALID_PLAY_COUNT_MIN_MESSAGE);
         }
     }
 }
